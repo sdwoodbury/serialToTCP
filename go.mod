a3b52f2244ad17b7b2a3c33c952816f2ca8bed90
Stuart Woodbury <woodburys@trxsystems.com>
2021-03-11 16:12:54 -0500

    simplified

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module user/serialToTCP
 go 1.15
 
 require (
-	github.com/WesleiRamos/tcp_client v0.0.0-20170319214905-d40aa84b451a // indirect
+	github.com/WesleiRamos/tcp_client v0.0.0-20170319214905-d40aa84b451a
 	github.com/albenik/go-serial v1.2.0
 	github.com/albenik/go-serial/v2 v2.3.0 // indirect
 	golang.org/x/sys v0.0.0-20210301091718-77cc2087c03b // indirect
--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
-	"github.com/albenik/go-serial"
+	client "github.com/WesleiRamos/tcp_client"
+	serial "github.com/albenik/go-serial"
 )
 
 func readWrite(port serial.Port, tcpToSensorChan chan []byte, sensorToTcpChan chan []byte, shouldQuit chan bool) {
 
-	buff := make([]byte, 100)
+	buff := make([]byte, 128)
 	readSerial := func() {
 		n, err := port.Read(buff)
 		if err != nil {
 			log.Fatal(err)
 		}
 		sensorToTcpChan <- buff[:n]
-		fmt.Printf("%v", string(buff[:n]))
+		//fmt.Printf("%v", string(buff[:n]))
 	}
 
 	for {
@@ -32,84 +33,51 @@ func readWrite(port serial.Port, tcpToSensorChan chan []byte, sensorToTcpChan ch
 			port.Write(incoming)
 		default:
 			readSerial()
+			time.Sleep(1 * time.Millisecond)
 		}
 	}
 }
 
-func netMgr(tcpToSensorChan chan []byte, sensorToTcpChan chan []byte, serverAddress string, sensorToTcpPort int, tcpToSensorPort int, shouldQuit chan bool) {
+func netMgr(tcpToSensorChan chan []byte, sensorToTcpChan chan []byte, tcpToSensorSocket *client.Connection, sensorToTcpSocket *client.Connection, shouldQuit chan bool) {
 
-	sensorToTcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", serverAddress, sensorToTcpPort))
-	if err != nil {
-		log.Fatal(err)
-	}
+	sensorToTcpSocket.OnOpen(func() {
+		fmt.Println("sensor to tcp socket connected")
+	})
 
-	tcpToSensorAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", serverAddress, tcpToSensorPort))
-	if err != nil {
-		log.Fatal(err)
-	}
+	tcpToSensorSocket.OnOpen(func() {
+		fmt.Println("tcp to sensor socket connected")
+	})
 
-	sensorToTcpConn, err := net.DialTCP("tcp", nil, sensorToTcpAddr)
-	defer sensorToTcpConn.Close()
-	if err != nil {
-		fmt.Printf("error opening sensorToTcp port")
-		log.Fatal(err)
-	}
+	tcpToSensorSocket.OnMessage(func(message []byte) {
+		tcpToSensorChan <- message
+	})
 
-	tcpToSensorConn, err := net.DialTCP("tcp", nil, tcpToSensorAddr)
-	defer tcpToSensorConn.Close()
-	if err != nil {
-		fmt.Println("error opening tcpToSensor port")
-		log.Fatal(err)
-	}
-
-	readTcpWriteSensor := func() {
-		buf := make([]byte, 1024)
-		reqLen, err := tcpToSensorConn.Read(buf)
-		if err == nil {
-			tcpToSensorChan <- buf[:reqLen]
-		}
-	}
+	sensorToTcpSocket.Listen()
 
 	for {
 		select {
 		case <-shouldQuit:
 			return
 		case toWrite := <-sensorToTcpChan:
-			sensorToTcpConn.Write(toWrite)
+			if sensorToTcpSocket.Connected {
+				sensorToTcpSocket.Write(toWrite)
+			}
 		default:
-			readTcpWriteSensor()
+			time.Sleep(1 * time.Millisecond)
 		}
 	}
 }
 
-func mainwork(shouldQuit chan bool, comPort string, serverAddress string, sensorToTcpPort int, tcpToSensorPort int) {
-	ports, err := serial.GetPortsList()
+func getPort(comPort string) serial.Port {
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open(comPort, mode)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
-	}
-	for _, portName := range ports {
-		if portName != comPort {
-			continue
-		}
-		fmt.Printf("Found port: %v\n", portName)
 
-		mode := &serial.Mode{
-			BaudRate: 115200,
-		}
-		port, err := serial.Open(portName, mode)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tcpToSensorChan := make(chan []byte)
-		sensorToTcpChan := make(chan []byte)
-		go netMgr(tcpToSensorChan, sensorToTcpChan, serverAddress, tcpToSensorPort, sensorToTcpPort, shouldQuit)
-		go readWrite(port, tcpToSensorChan, sensorToTcpChan, shouldQuit)
-		break
-	}
+	return port
 }
 
 func main() {
@@ -126,7 +94,15 @@ func main() {
 	tcpToSensorPort, _ := 8001, 1  //strconv.Atoi(os.Args[3]) // the server has ports as follows: readSerial, writeSerial
 	sensorToTcpPort := tcpToSensorPort + 1
 	shouldQuit := make(chan bool)
-	go mainwork(shouldQuit, comPort, serverAddress, sensorToTcpPort, tcpToSensorPort)
+
+	sensorToTcpSocket := client.New(fmt.Sprintf("%s:%d", serverAddress, sensorToTcpPort))
+	tcpToSensorSocket := client.New(fmt.Sprintf("%s:%d", serverAddress, tcpToSensorPort))
+	sensorPort := getPort(comPort)
+
+	tcpToSensorChan := make(chan []byte)
+	sensorToTcpChan := make(chan []byte)
+	go readWrite(sensorPort, tcpToSensorChan, sensorToTcpChan, shouldQuit)
+	go netMgr(tcpToSensorChan, sensorToTcpChan, sensorToTcpSocket, tcpToSensorSocket, shouldQuit)
 
 	<-signalChan
 
